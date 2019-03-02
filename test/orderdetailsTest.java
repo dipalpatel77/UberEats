b37b292fb62baf98177a6d8eee7d20b3f01ae0a6
1895269 <1895269@E21028.CEGEPGIM.LCL>
2019-03-02 12:36:19 -0500

    created order test of orederdetails

---
+++ test/orderdetailsTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1895269
+ */
+public class orderdetailsTest {
+    
+    public orderdetailsTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+     @Test
+     public void createIdTest()
+    {
+        System.out.println("Test id for createId method in Order Class");         
+        
+        Date date = new Date();
+        String name = "";
+        String expectedResult = "19-Mar-E-XX";
+        String realResult = ubereats.Order.createId(date, name);
+        assertEquals(expectedResult, realResult);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
