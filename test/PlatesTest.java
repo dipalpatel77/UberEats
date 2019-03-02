ded36483540e059067b0943f71d05c43a2752685
1895277 <1895277@E21017.CEGEPGIM.LCL>
2019-03-02 12:30:59 -0500

    Added unit test case for Plates class.

---
+++ test/PlatesTest.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Date;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author 1895277
+ */
+public class PlatesTest {
+    
+    public PlatesTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+       @Test
+    public void createIdTest()
+    {
+        System.out.println("Test id for createId method in Plates Class");         
+        
+        Date date = new Date();
+        String name = "Indian";
+        String expectedResult = "19-Mar-E-IN";
+        String realResult = ubereats.Plates.createId(date, name);
+        assertEquals(expectedResult, realResult);
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
